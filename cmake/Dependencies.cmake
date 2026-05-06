c3817dee105a0a89a81a24d30e695de6c222a07b
heimdallr <heimdalrnsk@gmail.com>
2026-05-06 15:06:46 +0700

    #1207 Хватит качать этот файл. Пусть лежит готовый.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,25 +1,4 @@
-# Downloading the CPM.cmake package manager
-set( CPM_DOWNLOAD_VERSION 0.42.0 )
-set( CPM_DOWNLOAD_HASH "2020b4fc42dba44817983e06342e682ecfc3d2f484a581f11cc5731fbe4dce8a" )
-if( CPM_SOURCE_CACHE )
-    set( CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake" )
-elseif( DEFINED ENV{CPM_SOURCE_CACHE} )
-    set( CPM_DOWNLOAD_LOCATION "$ENV{CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake" )
-else()
-    set( CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake" )
-endif()
-get_filename_component( CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE )
-set( CPM_USE_NAMED_CACHE_DIRECTORIES ON )
-if( NOT ( EXISTS ${CPM_DOWNLOAD_LOCATION} ) )
-    message( STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}" )
-    file( DOWNLOAD
-          https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
-          ${CPM_DOWNLOAD_LOCATION}
-          SHOW_PROGRESS
-          EXPECTED_HASH SHA256=${CPM_DOWNLOAD_HASH}
-    )
-endif()
-include( ${CPM_DOWNLOAD_LOCATION} )
+include( cmake/CPM.cmake )
 
 if( BIT7Z_CUSTOM_7ZIP_PATH STREQUAL "" )
     # 7-zip source code
